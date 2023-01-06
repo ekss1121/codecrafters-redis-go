c52b494fc522640b40a91ced72874c9504d39947
Zhihang Liu <ekss1121@gmail.com>
2023-01-05 20:30:58 -0500

    multiple ping

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -9,7 +11,7 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Redis-go server started listening on port 6379!")
 
 	// Uncomment this block to pass the first stage
 	//
@@ -19,12 +21,21 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Accept connections forever
 	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer conn.Close()
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connection received from: ", conn.RemoteAddr().String())
+		// write a response using conn
+		conn.Write([]byte("+PONG\r\n"))
 	}
 
-	// write a response using conn
-	conn.Write([]byte("+PONG\r\n"))
 }
