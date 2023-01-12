10758575b3f499e61c7715e8890d7022ad0cd65a
Zhihang Liu <ekss1121@gmail.com>
2023-01-11 22:13:51 -0500

    expire at debug

--- app/server.go
+++ app/server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io"
+	"strconv"
 	"sync"
+	"time"
 
 	// Uncomment this block to pass the first stage
 	"net"
@@ -17,13 +19,19 @@ var (
 	GET_COMMAND  = "get"
 )
 
+type data struct {
+	Value    string
+	Px       time.Duration
+	ExpireAt time.Time
+}
+
 type cache struct {
-	Table map[string]string
+	Table map[string]*data
 	Mu    sync.RWMutex
 }
 
 var localCache = &cache{
-	Table: make(map[string]string),
+	Table: make(map[string]*data),
 }
 
 func handleRequest(conn net.Conn) {
@@ -53,7 +61,15 @@ func handleRequest(conn net.Conn) {
 		} else if bukLen == 2 && commandBuks[0] == ECHO_COMMAND { // Echo command
 			handleEchoCommand(commandBuks[1], conn)
 		} else if bukLen == 3 && commandBuks[0] == SET_COMMAND { // Set command
-			handleSetCommand(commandBuks[1], commandBuks[2], conn)
+			handleSetCommand(commandBuks[1], commandBuks[2], -1, conn)
+		} else if bukLen == 5 && commandBuks[0] == SET_COMMAND {
+			exp, err := strconv.Atoi(commandBuks[4])
+			if err != nil || commandBuks[3] != "px" {
+				conn.Write([]byte(fmt.Sprintf("-ERR syntax error\r\n")))
+				return
+			}
+
+			handleSetCommand(commandBuks[1], commandBuks[2], exp, conn)
 		} else if bukLen == 2 && commandBuks[0] == GET_COMMAND { // Get command
 			handleGetCommand(commandBuks[1], conn)
 		} else {
@@ -63,26 +79,44 @@ func handleRequest(conn net.Conn) {
 
 }
 
-func handleSetCommand(key string, value string, conn net.Conn) {
+func handleSetCommand(key string, value string, px int, conn net.Conn) {
 	localCache.Mu.Lock()
 	if _, found := localCache.Table[key]; found {
 		fmt.Println("key already exists, update it")
 	}
-	localCache.Table[key] = value
+	newV := &data{
+		Value:    value,
+		Px:       time.Duration(px),
+		ExpireAt: setNewExpireAt(time.Duration(px)),
+	}
+
+	localCache.Table[key] = newV
 	localCache.Mu.Unlock()
 	conn.Write([]byte("+OK\r\n"))
 }
 
 func handleGetCommand(key string, conn net.Conn) {
 	localCache.Mu.RLock()
-	if _, found := localCache.Table[key]; !found {
+	if v, found := localCache.Table[key]; !found {
 		localCache.Mu.RUnlock()
 		conn.Write([]byte("$-1\r\n"))
 		return
+	} else {
+		if v.ExpireAt.Before(time.Now()) {
+			localCache.Mu.RUnlock()
+			conn.Write([]byte("$-1\r\n"))
+			return
+		}
 	}
-	value := localCache.Table[key]
+	newV := &data{
+		Value:    localCache.Table[key].Value,
+		Px:       localCache.Table[key].Px,
+		ExpireAt: setNewExpireAt(localCache.Table[key].Px),
+	}
+
+	localCache.Table[key] = newV
 	localCache.Mu.RUnlock()
-	conn.Write([]byte(fmt.Sprintf("+%s\r\n", value)))
+	conn.Write([]byte(fmt.Sprintf("+%s\r\n", newV.Value)))
 }
 
 func handlePingCommand(conn net.Conn) {
@@ -111,6 +145,14 @@ func parseRedisCommand(command string) []string {
 	commands = append(commands, curCommand)
 	restCommand := parseRedisCommand(command[i+2+bukLen+2:])
 	return append(commands, restCommand...)
+}
+
+func setNewExpireAt(px time.Duration) time.Time {
+	newExp := time.Now().Add(px * time.Millisecond)
+	if px == -1 {
+		return time.Now().Add(time.Hour * 24 * 365 * 100)
+	}
+	return newExp
 }
 
 func main() {
